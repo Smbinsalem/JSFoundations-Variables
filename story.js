60d83aa4c9e772877599c78e07daa3a341c4933e
SmbinSalem <you@example.com>
2021-02-02 19:39:56 +0300

    Here we are againgit add .!

---
+++ story.js
@@ -0,0 +1,5 @@
+let age = 26
+let name = "Salem"
+let hobby = "Socializing"
+
+console. log (`Mr./Mrs. ${name}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`)
